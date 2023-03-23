f3d5bf60ffd25f69905eceed6ff9af879e2fcfda
kamilcal <kamilcaal@gmail.com>
2023-03-23 14:27:31 +0300

    ListViewController created
    tableView added

---
+++ PokemonApp/ListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListViewController.swift
+//  PokemonApp
+//
+//  Created by kamilcal on 23.03.2023.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
