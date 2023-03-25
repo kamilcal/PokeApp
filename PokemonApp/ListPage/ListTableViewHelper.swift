64b709871b827dd7bf5226943767df8c73768942
kamilcal <kamilcaal@gmail.com>
2023-03-25 12:42:04 +0300

    UI improvements

--- PokemonApp/ListPage/ListTableViewHelper.swift
+++ PokemonApp/ListPage/ListTableViewHelper.swift
@@ -28,60 +28,19 @@ class ListTableViewHelper: NSObject{
         super.init()
         
         setupTableView()
-
+        
+        
         viewModel.getPokemonList(completion: {
             self.tableView?.reloadData()
         })
     }
     private func setupTableView() {
-        tableView?.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "Header")
         tableView?.register(.init(nibName:  "ListTableViewCell", bundle: nil), forCellReuseIdentifier: cellIdentifier)
-        tableView?.sectionHeaderHeight = 100
         tableView?.delegate = self
         tableView?.dataSource = self
-        
+
     }
     
-    //    MARK: - Header
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "Header") ?? UITableViewHeaderFooterView(reuseIdentifier: "Header")
-        
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.image = UIImage(named: "pokemon")
-        
-        
-        headerView.contentView.addSubview(imageView)
-        
-        NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 16),
-            imageView.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 20),
-            imageView.widthAnchor.constraint(equalToConstant: 60),
-            imageView.heightAnchor.constraint(equalToConstant: 60),
-            imageView.bottomAnchor.constraint(equalTo: headerView.bottomAnchor, constant: -20)
-        ])
-        
-        let headerLabel = UILabel()
-        headerLabel.translatesAutoresizingMaskIntoConstraints = false
-        headerLabel.text = "Pokémon App"
-        headerLabel.textColor = .black
-        headerLabel.font = UIFont(name: "Futura-Bold", size: 35)
-        headerView.contentView.addSubview(headerLabel)
-        headerLabel.font = UIFont.boldSystemFont(ofSize: 35)
-        
-        
-        NSLayoutConstraint.activate([
-            headerLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 12),
-            headerLabel.trailingAnchor.constraint(equalTo: headerView.contentView.trailingAnchor, constant: -16),
-            headerLabel.topAnchor.constraint(equalTo: headerView.contentView.topAnchor, constant: 20),
-            headerLabel.bottomAnchor.constraint(equalTo: headerView.contentView.bottomAnchor, constant: -20)
-        ])
-        
-        headerView.contentView.backgroundColor = .clear
-        
-        return headerView
-    }
 }
 
 //    MARK: - Delegate - Datasource
