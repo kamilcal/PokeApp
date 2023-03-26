741e23c12cf8b96febc0025c518d8ff8071803fd
kamilcal <kamilcaal@gmail.com>
2023-03-26 14:47:46 +0300

    UI improvement

--- PokemonApp/DetailPage/DetailViewController.swift
+++ PokemonApp/DetailPage/DetailViewController.swift
@@ -15,7 +15,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var abilitiesTitle: UILabel!
-    @IBOutlet weak var pokeView: UIImageView!
+    @IBOutlet weak var abilitiesTitleLabel: UILabel!
     
     private var viewModel = DetailViewModel()
     
@@ -30,6 +30,13 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.delegate = self
+        gradientUI()
+    }
+    private func gradientUI(){
+        let gradientView = Gradient(color: [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1),#colorLiteral(red: 1, green: 0.5409764051, blue: 0.8473142982, alpha: 1)])
+        gradientView.frame = view.bounds
+        view.addSubview(gradientView)
+        view.sendSubviewToBack(gradientView)
     }
 }
 
